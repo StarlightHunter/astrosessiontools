5847a9c7849c90854c2689d125945534e33a49f4
Oliver Gutierrez <ogutsua@gmail.com>
2022-07-26 12:06:55 +0100

    Added dummy test

---
+++ tests/dummy.py
@@ -0,0 +1,13 @@
+"""Dummy test."""
+
+import unittest
+
+
+class TestDummy(unittest.TestCase):
+    """Dummy test."""
+
+    def setUp(self):
+        """Define setup for test class."""
+
+    def dummy_test(self, mock_argparse):
+        """Test nothing."""
