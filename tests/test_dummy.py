fc330929766fd45c3cca9ce363878c0e116e16c4
Oliver Gutierrez <ogutsua@gmail.com>
2022-07-27 12:30:37 +0100

    Fixed tests and coverage

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -6,8 +6,6 @@ import unittest
 class TestDummy(unittest.TestCase):
     """Dummy test."""
 
-    def setUp(self):
-        """Define setup for test class."""
-
-    def dummy_test(self, mock_argparse):
+    def test_dummy_test(self):
         """Test nothing."""
+        self.assertTrue((True))
